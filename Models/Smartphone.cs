acf7dd256e7a3ea655db8648043d7c8a3259d855
GiovaniNeri <giovanikelvin01@gmail.com>
2022-09-28 13:45:06 -0300

    primeiro commit

---
+++ Models/Smartphone.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio_POO.Models
+{
+    public abstract class Smartphone
+    {
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+
+        public string Modelo { get; set; }
+
+        public string IMEI { get; set; }
+
+        public int Memoria { get; set; }
+
+
+        public Smartphone(string numero, string modelo, string imei, int memoria)
+        {
+            Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
+        }
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando...");
+        }
+
+        public void ReceberLigacao()
+        {
+            Console.WriteLine("Recebendo ligação...");
+        }
+
+        public abstract void InstalarAplicativo(string nomeApp);
+    }
+}
